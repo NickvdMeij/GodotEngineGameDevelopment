013c184cee673b6dea90f6f111dae8370a43b065
N.L.P. van der Meij <n.vandermeij@make-it.business>
2018-04-19 22:09:34 +0200

    Initial commit. Implemented first 3 hours of try it yourselfs and exercises.

---
+++ hour_03/ScriptNode.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+func _ready():
+	
+	#var transform = Transform2D()
+	#var x = transform.x # X Axis Vector
+	#var y = transform.y # Y Axis Vector
+	#var o = transform.o # Origin translation vector
+	
+	#var rotate_matrix = Transform2D().rotated(deg2rad(45)) # rotate 45 degrees
+	#var scale_matrix = Transform2D().scaled(Vector2D(2,2)) # scale twice in each axis
+	#var translate_matrix = Transform2D().translated()
+	#var combined_matrix = translate_matrix * rotate_matrix * scale_matrix # combine the transforms in order
+	
+	#$ScriptNode.transform *= combined;
+	
+	pass
+
+func _process(delta):
+	pass
